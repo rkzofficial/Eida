d831811bde75a760dbafbf2f8f033c28ec99febf
Kir4Kun <rkbl4ze@gmail.com>
2022-04-09 00:21:29 +0530

    Added Auth and SigninForm Bloc

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -7,14 +7,20 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
+import 'application/auth/auth_bloc.dart' as _i3;
+import 'application/auth/sign_in_form/sign_in_form_bloc.dart' as _i6;
+import 'domain/auth/i_auth_facade.dart' as _i4;
 import 'infrastructure/auth/firebase_user_mapper.dart'
-    as _i3; // ignore_for_file: unnecessary_lambdas
+    as _i5; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.lazySingleton<_i3.FirebaseUserMapper>(() => _i3.FirebaseUserMapper());
+  gh.factory<_i3.AuthBloc>(() => _i3.AuthBloc(get<_i4.IAuthFacade>()));
+  gh.lazySingleton<_i5.FirebaseUserMapper>(() => _i5.FirebaseUserMapper());
+  gh.factory<_i6.SignInFormBloc>(
+      () => _i6.SignInFormBloc(get<_i4.IAuthFacade>()));
   return get;
 }
