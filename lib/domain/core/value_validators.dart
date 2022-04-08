386ae5a036cc700fc26af7f2f0020a51fadd7594
Kir4Kun <rkbl4ze@gmail.com>
2022-04-08 23:08:33 +0530

    Added Value Objects and Core Domain

---
+++ lib/domain/core/value_validators.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:eida/domain/core/failures.dart';
+
+Either<ValueFailure<String>, String> validateEmailAddress(String input) {
+  const emailRegex =
+      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+
+  if (RegExp(emailRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
 
 import 'presentation/app_widget.dart';
 
 /// The entry point of the application.
 void main() {
+  Either<int, String> either = right('Hello');
+
   runApp(const App());
 }
